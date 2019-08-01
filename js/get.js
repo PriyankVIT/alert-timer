e5845d53d7060b0c747f0c2a6de2d8a79601d09f
priyankVIT <priyankkaushik13@gmail.com>
2019-08-01 14:11:01 +0530

    Day Add

--- js/get.js
+++ js/get.js
@@ -4,11 +4,13 @@ $(document).ready(() => {
 
 $("#btn").on("click", (event) => {
 	event.preventDefault()
+	let days = $("#day").val()
 	let hours = $("#hour").val()
 	let mins = $("#min").val()
 	let secs = $("#sec").val()
-	console.log(hours, mins, secs)
+	console.log(days, hours, mins, secs)
 
+	window.localStorage.setItem("d", days)
 	window.localStorage.setItem("h", hours)
 	window.localStorage.setItem("m", mins)
 	window.localStorage.setItem("s", secs)
--- js/timer.js
+++ js/timer.js
@@ -1,11 +1,13 @@
-
 function startTimer(duration,display) {
-    var timer = duration, hours, minutes, second;
+    var timer = duration, days, hours, minutes, second;
+
     var interval = setInterval(function () {
-        hours   = parseInt(timer / 3600, 10);
-        minutes = parseInt((timer / 60)-(hours*60), 10);
+        days = parseInt((timer / (24*60*60)), 10);
+        hours   = parseInt((timer / (60*60)) - (days*24), 10);
+        minutes = parseInt((timer / (60)) - (hours*60) - (days*24*60), 10);
         seconds = parseInt(timer % 60, 10);
 
+        days = days < 10 ? "0" + days : days;
         hours = hours < 10 ? "0" + hours : hours;
         minutes = minutes < 10 ? "0" + minutes : minutes;
         seconds = seconds < 10 ? "0" + seconds : seconds;
@@ -13,9 +15,11 @@ function startTimer(duration,display) {
         document.getElementById("mins").innerHTML = minutes;
         document.getElementById("seconds").innerHTML = seconds;
         document.getElementById("hours").innerHTML = hours;
+        document.getElementById("days").innerHTML = days;
 
         if (--timer < 0) {
             document.getElementById("up").innerHTML = "TIME UP";
+            document.getElementById("days").innerHTML = "00";
             document.getElementById("hours").innerHTML = "00";
             document.getElementById("mins").innerHTML = "00";
             document.getElementById("seconds").innerHTML = "00";
@@ -24,14 +28,17 @@ function startTimer(duration,display) {
     }, 1000);
 }
 window.onload = function () {  
+    let d = window.localStorage.getItem("d")
     let h = window.localStorage.getItem("h")
     let m = window.localStorage.getItem("m")
     let s = window.localStorage.getItem("s")
-    console.log(h, m, s);
+    console.log(d, h, m, s);
+    d = parseInt(d)
     h = parseInt(h)
     m = parseInt(m)
     s = parseInt(s)
-    var measure = (h*3600) + (m*60) + s;
+    console.log(d,h,m,s)
+    var measure =(d*24*60*60) + (h*60*60) + (m*60) + s;
     display = document.querySelector('#time');
     startTimer(measure,display);
 };
